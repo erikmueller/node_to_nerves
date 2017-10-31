a460096d257730d04cbe2729a32b6366e9df4e57
Erik Müller <e.mueller.mail@gmail.com>
2017-10-31 14:35:35 +0100

    initial commit

---
+++ lib/node_to_nerves.ex
@@ -0,0 +1,3 @@
+defmodule NodeToNerves do
+  
+end
---
+++ test/node_to_nerves_test.exs
@@ -0,0 +1,8 @@
+defmodule NodeToNervesTest do
+  use ExUnit.Case
+  doctest NodeToNerves
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
